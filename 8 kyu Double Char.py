3ab4aff67c3d49cf2f66372c81359e890805442a
JustinMNIC <104854769+JustinMNIC@users.noreply.github.com>
2024-02-07 16:32:01 +0300

    02/07/2024

---
+++ 8 kyu Double Char.py
@@ -0,0 +1,13 @@
+"""Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+Good Luck!"""
+
+def double_char(s):
+    ans = ""
+    for char in s:
+        ans += char * 2
+    return ans
